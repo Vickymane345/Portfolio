17e61e1e789a7f1c42047590b3a7789b6dd948d4
Vickymane345 <113917597+Vickymane345@users.noreply.github.com>
2025-04-20 15:48:26 +0100

    Update Skills.tsx

--- my-app/src/Components/SKills/Skills.tsx
+++ my-app/src/Components/SKills/Skills.tsx
@@ -60,7 +60,7 @@ export default function Skills() {
   useEffect(() => {
     
     setCurrentIndex(0); 
-    const interval = setInterval(nextSlide, 4000); 
+    const interval = setInterval(nextSlide, 1800); 
     return () => clearInterval(interval); 
   }, []);
   const context = useContext(UserContext);
